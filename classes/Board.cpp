d89b88e3eee1fb0e604897ac2548794b9bc017b9
Peter Schneider <peter@peterPad.(none)>
2013-12-30 01:08:23 +0100

    Adds debug output for Board.

--- classes/Board.cpp
+++ classes/Board.cpp
@@ -29,6 +29,29 @@ void Board::loadFen(std::string fen)
 
 std::string Board::getDump()
 {
-	// TODO: produce dump
-	return "";
+	std::string result = "";
+	long long figures = this->whites | this->blacks;
+	for (int x=0; x<=8; x++) {
+		for (int y=0; y<=8; y++) {
+			char f = '?';
+			int bit = y*8 + x;
+			long long position = 1 << bit;
+			if (position & figures) {
+				if (position & kings) f = 'W';
+				if (position & queens) f = 'Q';
+				if (position & rooks) f = 'R';
+				if (position & knights) f = 'K';
+				if (position & bishops) f = 'B';
+				if (position & pawns) f = 'P';
+				if (position & blacks) f = tolower(f);
+			}
+			else
+			{
+				f = '.';
+			}
+			result.append(f);
+		}
+		result.append(std::endl);
+	}
+	return result;
 }
