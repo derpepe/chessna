f7466de38540e758011e88c25329e464bdd08bf7
Peter Schneider <schneider.pj@gmail.com>
2025-08-15 19:39:07 +0200

    Validate command parameters

--- classes/Lib.cpp
+++ classes/Lib.cpp
@@ -6,7 +6,10 @@ std::vector<std::string> &Lib::split(const std::string &s, char delim, std::vect
     std::string item;
     while(std::getline(ss, item, delim))
     {
-        elems.push_back(item);
+        if (!item.empty())
+        {
+            elems.push_back(item);
+        }
     }
     return elems;
 }
