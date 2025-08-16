bb1d817d0f9c64d81b05588e5af202f6cfce714b
Peter Schneider <peter@codekunst.com>
2025-08-16 19:34:44 +0200

    center is more important now

--- classes/Evaluation.cpp
+++ classes/Evaluation.cpp
@@ -28,21 +28,21 @@
          unsigned long long centralMask = (1ULL << 27) | (1ULL << 28) | (1ULL << 35) | (1ULL << 36);
          int whiteCentral = __builtin_popcountll(board.pawns & board.whites & centralMask);
          int blackCentral = __builtin_popcountll(board.pawns & board.blacks & centralMask);
-         score += 10 * whiteCentral;
-         score -= 10 * blackCentral;
+         score += 15 * whiteCentral;
+         score -= 15 * blackCentral;
 
          // Penalize knights on the rim
          unsigned long long edgeMask = 0xff818181818181ffULL;
          int whiteEdgeKnights = __builtin_popcountll(board.knights & board.whites & edgeMask);
          int blackEdgeKnights = __builtin_popcountll(board.knights & board.blacks & edgeMask);
-        score -= 5 * whiteEdgeKnights;
-        score += 5 * blackEdgeKnights;
-
-        // Reward ability to castle
-        if (board.casteling_K || board.casteling_Q)
-                score += 20;
-        if (board.casteling_k || board.casteling_q)
-                score -= 20;
-
-        return score;
+         score -= 5 * whiteEdgeKnights;
+         score += 5 * blackEdgeKnights;
+         
+         // Reward ability to castle
+         if (board.casteling_K || board.casteling_Q)
+                 score += 20;
+         if (board.casteling_k || board.casteling_q)
+                 score -= 20;
+         
+         return score;
 }
