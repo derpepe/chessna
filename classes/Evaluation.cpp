c213390ae404d18a2b323b4ab8538de30452f464
Peter Schneider <peter@codekunst.com>
2025-08-16 19:38:34 +0200

    new weights

--- classes/Evaluation.cpp
+++ classes/Evaluation.cpp
@@ -28,29 +28,29 @@
          unsigned long long centralMask = (1ULL << 27) | (1ULL << 28) | (1ULL << 35) | (1ULL << 36);
          int whiteCentral = __builtin_popcountll(board.pawns & board.whites & centralMask);
          int blackCentral = __builtin_popcountll(board.pawns & board.blacks & centralMask);
-         score += 15 * whiteCentral;
-         score -= 15 * blackCentral;
+         score += 18 * whiteCentral;
+         score -= 18 * blackCentral;
 
          // Penalize knights and bishops on their starting squares
          unsigned long long whiteStartMask = (1ULL << 1) | (1ULL << 2) | (1ULL << 5) | (1ULL << 6);
          unsigned long long blackStartMask = (1ULL << 57) | (1ULL << 58) | (1ULL << 61) | (1ULL << 62);
          int whiteStartPieces = __builtin_popcountll((board.knights | board.bishops) & board.whites & whiteStartMask);
          int blackStartPieces = __builtin_popcountll((board.knights | board.bishops) & board.blacks & blackStartMask);
-         score -= 10 * whiteStartPieces;
-         score += 10 * blackStartPieces;
+         score -= 8 * whiteStartPieces;
+         score += 8 * blackStartPieces;
 
         // Penalize knights on the rim
         unsigned long long edgeMask = 0xff818181818181ffULL;
         int whiteEdgeKnights = __builtin_popcountll(board.knights & board.whites & edgeMask);
         int blackEdgeKnights = __builtin_popcountll(board.knights & board.blacks & edgeMask);
-         score -= 5 * whiteEdgeKnights;
-         score += 5 * blackEdgeKnights;
-         
-         // Reward ability to castle
-         if (board.casteling_K || board.casteling_Q)
-                 score += 20;
-         if (board.casteling_k || board.casteling_q)
-                 score -= 20;
-         
-         return score;
+        score -= 5 * whiteEdgeKnights;
+        score += 5 * blackEdgeKnights;
+        
+        // Reward ability to castle
+        if (board.casteling_K || board.casteling_Q)
+                score += 20;
+        if (board.casteling_k || board.casteling_q)
+                score -= 20;
+        
+        return score;
 }
